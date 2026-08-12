00290acbe9c52db1a46361a0c54d588879d33faa
erperninMngEnv <erpernin@MngEnvMCAP386131.onmicrosoft.com>
2026-08-12 13:00:54 +0200

    Committing 4 items from workspace 652a265f-c56c-4fcf-8872-e40d105886ac

---
+++ TranslyticalTaskFlow/translyticaludf.UserDataFunction/function_app.py
@@ -0,0 +1,41 @@
+import fabric.functions as fn
+import uuid
+
+udf = fn.UserDataFunctions()
+
+@udf.connection(argName="sqlDB",alias="translyticaldb") 
+@udf.function() 
+
+# Take a product description and product model ID as input parameters and write them back to the SQL database
+# Users will provide these parameters in the PowerBI report
+def write_one_to_sql_db(sqlDB: fn.FabricSqlConnection, productDescription: str, productModelId:int) -> str: 
+
+    # Error handling to ensure product description doesn't go above 200 characters
+    if(len(productDescription) > 200):
+        raise fn.UserThrownError("Descriptions have a 200 character limit. Please shorten your description.", {"Description:": productDescription})
+
+    # Establish a connection to the SQL database  
+    connection = sqlDB.connect() 
+    cursor = connection.cursor() 
+
+    # Insert data into the ProductDescription table  
+    insert_description_query = "INSERT INTO [SalesLT].[ProductDescription] (Description) OUTPUT INSERTED.ProductDescriptionID VALUES (?)" 
+    cursor.execute(insert_description_query, productDescription) 
+
+    # Get the result from the previous query 
+    results = cursor.fetchall() 
+
+    # In real-world cases, call an API to retrieve the cultureId
+    # For this example, generate a random Id instead
+    cultureId = str(uuid.uuid4()) 
+
+    # Insert data into the ProductModelProductDescription table 
+    insert_model_description_query = "INSERT INTO [SalesLT].[ProductModelProductDescription] (ProductModelID, ProductDescriptionID, Culture) VALUES (?, ?, ?);" 
+    cursor.execute(insert_model_description_query, (productModelId, results[0][0], cultureId[:6])) 
+
+    # Commit the transaction 
+    connection.commit() 
+    cursor.close() 
+    connection.close()  
+
+    return "Product description was added"
